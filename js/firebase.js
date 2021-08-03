3345bcde2ce2b34cc42f44503b58da2030481d0c
Francescocrivelli <francescoo.crivelli21@gmail.com>
2021-08-03 23:42:36 +0000

    Everything working V1

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+console.log(
+  "js/firebase.js : Remember to replace the instructor's firebase keys with yours!"
+);
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCsrvlbtXApg3ussj89EKWPimdt2fE9Trk",
+    authDomain: "watertrackerh20.firebaseapp.com",
+    databaseURL: "https://watertrackerh20-default-rtdb.firebaseio.com",
+    projectId: "watertrackerh20",
+    storageBucket: "watertrackerh20.appspot.com",
+    messagingSenderId: "958686492255",
+    appId: "1:958686492255:web:d28971bdfca0f499074dea"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
